afc96ef09effe70fedd77330c6ddc950541adfcd
Evgeniy Bukharin <buharinzxc123@gmail.com>
2023-08-18 15:09:27 +0500

    fixes

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,8 +1,8 @@
 import "./components/burger";
-import "./components/calculateProgress";
-import "./components/cols-hover-deals";
-import "./components/tableSlider-deals";
-import "./components/cols-hover-support";
-import "./components/tableSlider-support";
-import "./components/select";
-import "./components/horizontal-scroll-wheel";
+// import "./components/calculateProgress";
+// import "./components/cols-hover-deals";
+// import "./components/tableSlider-deals";
+// import "./components/cols-hover-support";
+// import "./components/tableSlider-support";
+// import "./components/select";
+// import "./components/horizontal-scroll-wheel";
